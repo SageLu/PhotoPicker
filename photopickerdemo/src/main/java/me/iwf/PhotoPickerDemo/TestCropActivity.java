1e2ef8d08034a806e29d19362d885a09ca2cbcf9
Administrator <sage.lu6@gmail.com>
2018-02-02 16:15:17 +0800

    添加测试Crop的Activity.

---
+++ photopickerdemo/src/main/java/me/iwf/PhotoPickerDemo/TestCropActivity.java
@@ -0,0 +1,13 @@
+package me.iwf.PhotoPickerDemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TestCropActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_crop);
+    }
+}
